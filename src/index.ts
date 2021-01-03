a355537eb3b60dfb4c6920fe748fc95366f6e457
Sycrixx <biometricmc@gmail.com>
2021-01-04 04:37:11 +1100

    Create ready event

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 import { config } from "dotenv";
 config();
 import { UpBeatClient } from "./struct/Client";
+import { ActivityType } from "discord.js";
 const client:UpBeatClient = new UpBeatClient({
     token: process.env.DISCORD_CLIENT_TOKEN!,
     prefix: process.env.DISCORD_CLIENT_PREFIX!,
+});
+
+// Ready Event
+client.once("ready", () => {
+   console.log(`Logged in as ${client.user?.tag ?? ""}`);
+
+   try {
+       void client.user?.setPresence({
+           status: "dnd",
+           activity: {
+               name: process.env.DISCORD_PRESENCE_MESSAGE,
+               // To read from env, we have to type case here
+               type: process.env.DISCORD_PRESENCE_TYPE as ActivityType,
+           },
+       });
+   }
+   catch (err) {
+       console.error(err);
+   }
 });
 
 // Client Login
